fd952252f9a7ba0f5a69eed697874577135ecb10
safimuhammad <safiwahid38@gmail.com>
2022-09-29 01:54:39 +0500

    test code

--- cube.py
+++ cube.py
@@ -6,3 +6,15 @@ from OpenGL.GL import *
 #starting the learning process
 #checking the shaders and vertices
 # trying the glsw
+
+#version 330 core
+# v in vec3 aVert;
+# w uniform mat4 uMVMatrix;
+# x uniform mat4 uPMatrix;
+# y out vec4 vCol;
+# void main() {
+# // apply transformations
+# z gl_Position = uPMatrix * uMVMatrix * vec4(aVert, 1.0);
+# // set color
+# { vCol = vec4(1.0, 0.0, 0.0, 1.0);
+# }
